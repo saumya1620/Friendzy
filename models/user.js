038bc189e4e2e4c86ef8e78e7098b0547b533db2
saumya1620 <122440288+saumya1620@users.noreply.github.com>
2024-09-02 19:35:36 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,13 +1,64 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema
-({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phoneNumber: { type: String, required: true },
-    password: { type: String, required: true },
-    gender: { type: String },
-    dob: { type: Date }
-}, { collection: 'user' });
+// import { Schema, model } from 'mongoose';
 
-module.exports = userSchema;
+const { text } = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/friendzy', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to user MongoDB');
+}).catch(err => {
+    console.error('Connection error', err);
+});
+
+
+
+const userschema = mongoose.Schema
+({
+    username: {
+        type: String,
+        required: true,  
+        trim: true,
+        unique: true       
+    },
+    email: {
+        type: String,
+        required: true,      
+        lowercase: true, 
+        trim: true,
+    },
+    phonenumber: {
+        type: Number,
+  
+        
+    },     
+    password: {
+        type: String,
+        required: true    
+    },
+    gender: {
+        type: String,
+        required: true,
+        lowercase: true, 
+        trim: true
+    },
+    date: {
+        type: Date,
+        
+    },
+    profilePicture: {
+        type: String, 
+        default: '/images/user.png' 
+    },
+    bio: { type: String, default: '' },
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user',default:[]}],
+    suggestions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }]  
+}, {timestamps : true},{ collection: 'user' });
+
+const usermodel = mongoose.model('user',userschema)
+module.exports = usermodel;
+
+// export default models('user',userschema)
 
